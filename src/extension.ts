14171b20a9c7fe0eadd447f9406d1168140fb226
Olivier Argentieri <olivierargentieri@gmail.com>
2023-10-13 19:57:21 +0000

    add extension

---
+++ src/extension.ts
@@ -0,0 +1,17 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { regexNavigation } from './modules/regexnavigation';
+
+export function activate(context: vscode.ExtensionContext) {
+	const modulesToLoad = [
+		regexNavigation
+	]
+
+	modulesToLoad.forEach((module) => {
+		module(context);
+	})
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
---
+++ src/modules/regexnavigation.ts
@@ -0,0 +1,103 @@
+
+import * as vscode from 'vscode';
+
+
+const getRegex = (languageID: string): RegExp | undefined => {
+    console.log(languageID);
+    switch (languageID) {
+        case 'python':
+            return /(def \w+[\(|:])/g;
+        default:
+            return undefined
+    }
+}
+
+// this module he used to navigate to the next and previous definition of a function
+const getNextDefinition = (document: vscode.TextDocument, current_position: vscode.Position): vscode.Range | undefined => {
+
+    const regex = getRegex(document.languageId);
+    if (!regex) {
+        vscode.window.showInformationMessage('Sorry, this language is not supported yet.');
+        return;
+    }
+
+	for (let line = current_position.line + 1; line < document.lineCount; line++) {
+		const text = document.lineAt(line).text;
+		if (/def \w+[\(|:]/g.test(text)) {
+			return new vscode.Range(new vscode.Position(line, 0), new vscode.Position(line, 0));
+		}
+	}
+}
+
+const getPreviousDefinition = (document: vscode.TextDocument, current_position: vscode.Position): vscode.Range | undefined => {
+    
+    const regex = getRegex(document.languageId);
+    if (!regex) {
+        vscode.window.showInformationMessage('Sorry, this language is not supported yet.');
+        return;
+    }
+
+	for (let line = current_position.line-1; line >= 0; line--) {
+		const text = document.lineAt(line).text;
+		if (/def \w+[\(|:]/g.test(text)) {
+			return new vscode.Range(new vscode.Position(line, 0), new vscode.Position(line, 0));
+		}
+	}
+}
+
+const revealNewDefinition = (editor: vscode.TextEditor, newDefiniton: vscode.Range | undefined) => {
+	if (!editor) {
+		return;
+	}
+
+	if (!newDefiniton) {
+		return;
+	}
+
+	const newPosition = new vscode.Position(newDefiniton.start.line, 0);
+	editor.selection = new vscode.Selection(newPosition, newPosition);
+
+	// also move view to the start of the function
+	editor.revealRange(newDefiniton);
+}
+
+export function regexNavigation(context: vscode.ExtensionContext) {
+    const previousDefCommand = vscode.commands.registerTextEditorCommand("vsharm.previousDef", async () => {
+		const editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			return;
+		}
+
+		const document = editor.document;
+		const position = editor.selection.active;
+		
+		const previousDef = getPreviousDefinition(document, position);
+		if (previousDef) {
+			// Move the cursor to the start of the nearest function.
+			const newPosition = new vscode.Position(previousDef.start.line, 0);
+			editor.selection = new vscode.Selection(newPosition, newPosition);
+
+			// also move view to the start of the function
+			editor.revealRange(previousDef);
+		}
+	})
+
+	const nextDefCommand = vscode.commands.registerTextEditorCommand("vsharm.nextDef", async () => {
+		const editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			return;
+		}
+
+		const document = editor.document;
+		const position = editor.selection.active;
+		
+		revealNewDefinition(editor, getNextDefinition(document, position))
+	})
+
+    context.subscriptions.push(
+        previousDefCommand,
+        nextDefCommand,
+    );
+}
